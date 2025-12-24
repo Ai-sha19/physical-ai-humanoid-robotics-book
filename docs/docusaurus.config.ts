e5a1c0dd8117430915d1293b05d021cc902cf341
Ai-sha19 <aisha.aysha216@gmail.com>
2025-12-24 07:40:42 +0500

    add url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: '/',
+  baseUrl: '/physical-ai-humanoid-robotics-book/',
 
   // GitHub pages deployment config.
   organizationName: 'Aisha',
